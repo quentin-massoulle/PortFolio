4ed1207a5d75950a5a73972eb61b996c217c44ae
quentin-massoulle <quentin.massoulledu80@gmail.com>
2024-10-18 14:31:28 +0200

    Ajoue de l

--- SitePotfolio/resources/views/layout.blade.php
+++ SitePotfolio/resources/views/layout.blade.php
@@ -6,9 +6,19 @@
         <link rel="stylesheet" href="./css/styles.css"/>
         @yield('styles')
     @yield('script')
+    <title>Quentin-Massoulle</title>
      </head>
     <body>
-        <div class="navbar">navbar</div>
+        <div class="navbar">
+            <div class ="element">
+                <a href="/"><img src="" alt="Photo moi"></a>
+                <a href="">A-Propos</a>
+                <a href="">Mon Parcours</a>
+                <a href="">Mes Passion</a>
+                <a href="">Mes Projet</a>
+                <a href="">Me Contacter</a>
+            </div>
+        </div>
         @yield('content')
         <div class="footer">footer</div>
     </body>
